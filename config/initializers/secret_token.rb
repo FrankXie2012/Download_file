6c4271cbf09658ec71a302f8327f5df73ce4c484
FrankXie2012 <frank@51shepherd.com>
2013-06-07 13:04:46 +0800

    init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DownloadPages::Application.config.secret_token = '069bbe6fc8a3ba4213a92f66e70e47704a250226d6921a4969ce52f853de8449ddf7e7cb5236785d3e515770beda87ecec9371bb42429473461dfd48e4619115'
